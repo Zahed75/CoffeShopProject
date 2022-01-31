b67296f84875c52cda46e1e8272ff3732802ddf4
Zahed75 <Zahedhasan.CS@gmail.com>
2022-01-31 21:08:55 +0600

    first commit

---
+++ Coffee_App/views.py
@@ -0,0 +1,25 @@
+from django.core import paginator
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect, redirect
+from django.views.generic import CreateView, UpdateView, ListView, DetailView, View, TemplateView, DeleteView
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib import messages
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.mail import send_mail, EmailMessage
+from django.contrib.auth.models import User
+from django.views.generic.edit import DeleteView
+from django.views import View
+from django.views import View
+
+
+# Create your views here.
+
+def index(request):
+    dict = {}
+
+    return render(request, 'Coffee_App/index.html', context=dict)
